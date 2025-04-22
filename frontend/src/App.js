490648882c520a6d0d9014045d44ebbf12e4c82f
OscarCespedes08 <oscaresteban.cespedes@gmail.com>
2025-04-22 08:15:48 -0500

    Corregir URL para descargas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,17 +43,5 @@ function App() {
 
 
 
-<Routes>
-  <Route path="/" element={<HomeScreen />} />
-  <Route path="/clients" element={<ClientList />} />
-  <Route path="/clients/new" element={<ClientForm />} />
-  <Route path="/clients/edit/:id" element={<ClientForm />} />
-  <Route path="/products" element={<ProductList />} />
-  <Route path="/products/new" element={<ProductForm />} />
-  <Route path="/products/edit/:id" element={<ProductForm />} />
-  <Route path="/sales" element={<SaleList />} />
-  <Route path="/sales/new" element={<SaleForm />} />
-  <Route path="/sales/:id" element={<SaleForm />} />
-</Routes>
 
 export default App;
